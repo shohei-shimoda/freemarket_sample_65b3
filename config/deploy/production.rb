7f8af39d49ef5dac199cc69722c0ce3cc3e5effb
shohei-shimoda <s.tel-9527@docomo.ne.jp>
2019-12-19 17:34:05 +0900

    d

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,4 @@
 server '54.64.93.92', user: 'ec2-user', roles: %w{app db web}
-
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
