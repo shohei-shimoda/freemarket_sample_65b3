4447d166c7a9f496bfd221340db183e6fad4949a
shohei-shimoda <s.tel-9527@docomo.ne.jp>
2019-12-19 17:47:50 +0900

    git

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ lock '3.11.2'
 set :application, 'freemarket_sample_65b3'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'shohei-shimoda/freemarket_sample_65b3.git'
+set :repo_url,  'git@github.com:shohei-shimoda/freemarket_sample_65b3.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
