383fb32b28c7853a3fe74fc810c7437700172865
shohei-shimoda <s.tel-9527@docomo.ne.jp>
2019-12-23 21:40:22 +0900

    logout削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'items#index'
   resources :items, only: [:index, :new, :show]
-  resources :users, only: [:show, :edit, :logout] do
+  resources :users, only: [:show, :edit] do
     collection do
       get 'logout'
     end
