0005b36e5e5f70efb0b625745c3660dd8a7542ff
shohei-shimoda <s.tel-9527@docomo.ne.jp>
2019-12-23 11:42:08 +0900

    routes変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "items#new"
+  root "items#index"
+  resources :items, only: [:index, :new]
+  resources :users, only: [:edit, :show]
 end
