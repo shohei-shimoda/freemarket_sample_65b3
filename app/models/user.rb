7adfc636d8f79d1cd223048201ff713e28d264cf
saki1008 <57945471+saki1008@users.noreply.github.com>
2020-01-07 10:41:10 +0900

    a

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,52 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable,omniauth_providers: %i[facebook google]
+  has_many :sns_credentials, dependent: :destroy
+
+  def self.find_oauth(auth)
+    uid = auth.uid
+    provider = auth.provider
+    snscredential = SnsCredential.where(uid: uid, provider: provider).first
+    # binding.pry
+
+    if snscredential.present? #sns登録のみ完了してるユーザー
+      user = User.where(id: snscredential.user_id).first
+      unless user.present? #ユーザーが存在しないなら
+        user = User.new(
+          # snsの情報
+          # binding.pry => auth.infoとかで確認 
+          nickname: auth.info.name,
+          email: auth.info.email
+        )
+      end
+      sns = snscredential
+      #binding.pry
+
+    else #sns登録 未
+      user = User.where(email: auth.info.email).first
+      if user.present? #会員登録 済
+        sns = SnsCredential.new(
+          uid: uid,
+          provider: provider,
+          user_id: user.id
+        )
+      else #会員登録 未
+        user = User.new(
+          nickname: auth.info.name,
+          email: auth.info.email
+        )
+        # binding.pry
+        sns = SnsCredential.create(
+          uid: uid,
+          provider: provider
+        )
+        # binding.pry 
+      end
+    end
+    # binding.pry
+    # hashでsnsのidを返り値として保持しておく
+    return { user: user , sns_id: sns.id }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+  controllers: { omniauth_callbacks: 'users/omniauth_callbacks',
+                 registrations: 'users/registrations' }         
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'items#index'
   resources :items, only: [:index, :new, :show]
